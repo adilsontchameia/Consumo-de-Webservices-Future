9a1763b67e5a63af38a70304d187aab7522a9aae
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-21 23:34:20 +0100

    criacao do metodo _recuperarPreco com Fututure

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,15 +9,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  //
-  void _recuperarPreco() async {
+  //Metodo para recuperar o preco
+  Future<Map> _recuperarPreco() async {
+    //Pegando a URL
     String url = "https://blockchain.info/ticker";
     http.Response response = await http.get(url);
-   
+    //Fututre - Fica mais facil gerenciar o consumo WEB
+    //Future - Conseguimos controlar a demora da requisicao e se deu certo.
+    return json.decode(response.body);
+
+    //Future - Dados que ainda vai se recuperar.
+    //Snapshot - Dados recuperados ao fazer a requisicao.
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          //Estado nulo da conexao
+          case ConnectionState.none:
+        }
+      },
+    );
   }
 }
