ab205dc932c62638e04e8f5aa4d232cae9c91e78
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-22 12:46:36 +0100

    criado corpo do app

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,40 +9,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Future<Map> _recuperarPreco() async {
+  /*Future<Map> _recuperarPreco() async {
+    
     String url = "https://blockchain.info/ticker";
     http.Response response = await http.get(url);
     return json.decode(response.body);
+    
   }
+*/
 
+  String urlBase = "https://jsonplaceholder.typicode.com";
+  //Metodo para recuperar postagens
+  _recuperarPostagens() {}
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: (context, snapshot) {
-        String resultado;
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-            print("conexao waiting");
-            resultado = "Carregando...";
-            break;
-          case ConnectionState.active:
-          case ConnectionState.done:
-            print("conexao done");
-            if (snapshot.hasError) {
-              resultado = "Erro ao carregar os dados.";
-            } else {
-              double valor = snapshot.data["BRL"]["buy"]; //Aqui
-              resultado = "Preço do bitcoin: ${valor.toString()} ";
-            }
-            break;
-        }
-
-        return Center(
-          child: Text(resultado),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de Servico Avancado"),
+      ),
+      body: FutureBuilder<Map>(
+        future: _recuperarPostagens(),
+        builder: (context, snapshot) {
+          //String resultado;
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              print("conexao waiting");
+              //resultado = "Carregando...";
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+              print("conexao done");
+              if (snapshot.hasError) {
+                //resultado = "Erro ao carregar os dados.";
+              } else {
+                double valor = snapshot.data["BRL"]["buy"]; //Aqui
+                //resultado = "Preço do bitcoin: ${valor.toString()} ";
+              }
+              break;
+          }
+          return Center(
+            child: Text(resultado),
+          );
+        },
+      ),
     );
   }
 }
